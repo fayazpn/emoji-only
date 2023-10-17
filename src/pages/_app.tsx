09d7724d4ee6482432d0a733decaa2ac37bdb85e
Fayaz Basheer <pnfayaz@gmail.com>
2023-10-17 16:31:46 +0400

    common headers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Social Emoji</title>
+        <meta name="description" content="🐦" />
+        <link rel="icon" href="🐦" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
