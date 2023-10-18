b8f11019deef84329182ace7d1449de93d47646f
Fayaz Basheer <pnfayaz@gmail.com>
2023-10-18 10:31:40 +0400

    server side helper for prefetching

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,21 +1,54 @@
-import Head from "next/head"
-import {type NextPage } from "next";
+import Head from "next/head";
+import { type GetStaticPropsContext, type NextPage } from "next";
 import { api } from "~/utils/api";
+import { appRouter } from "~/server/api/root";
 
-const ProfilePage: NextPage = () => {
-  const user = api.profile.getUserByUsername.useQuery({username: 'fayazpn'})
+const ProfilePage: NextPage<{username: string}> = ({username}) => {
+  const user = api.profile.getUserByUsername.useQuery({ username });
 
-  console.log(user.data)
-    return (
-      <>
+  return (
+    <>
       <Head>
-        <title>Profile</title>
+        <title>{username}</title>
       </Head>
-        <main className="flex justify-center">
-            <div>Posts by id</div>
-        </main>
-      </>
-    );
-  }
+      <main className="flex justify-center">
+        <div>Posts by id</div>
+      </main>
+    </>
+  );
+};
 
-  export default ProfilePage
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { db } from "~/server/db";
+import superjson from "superjson";
+
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ slug: string }>,
+) => {
+  const ssg = createServerSideHelpers({
+    router: appRouter,
+    ctx: { db: db, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug
+
+  if (typeof slug !== "string") throw new Error("no slug")
+
+  const username = slug.replace("@", "")
+
+  await ssg.profile.getUserByUsername.prefetch({username: username})
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      username
+    }
+  }
+};
+
+export const getStaticPaths = () => {
+  return {paths:[], fallback: "blocking"}
+}
+
+export default ProfilePage;
